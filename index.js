3b42abe335b3273fd372a17786f4d9cdf612f65b
Nick Junius <njunius@users.noreply.github.com>
2020-08-08 20:34:50 -0700

    updated Nick's grammar and added Uel's new ads to the canned and post game commentary

--- index.js
+++ index.js
@@ -163,7 +163,8 @@ const nickCommentaryGrammar = tracery.createGrammar({
     "#teamcommentary#", "#teamcommentary#", "#teamcommentary#", "#teamcommentary#",
     "#teamcommentary#", "#teamcommentary#",
     "I can't believe what I'm watching", "This is getting intense",
-    "In this processor we love and respect the shard", "Let us enjoy the game comrades"
+    "In this processor we love and respect the shard", "Let us enjoy the game comrades",
+    "Praise the solar eclipse"
   ],
   "teamcommentary":[
     "These teams are #descriptor#", "These teams #relationship# #whoinrelationship#",
@@ -174,7 +175,7 @@ const nickCommentaryGrammar = tracery.createGrammar({
   "descriptor":["unbelievable", "so evenly matched", "so splortsperson like"],
   "relationship":[
     "respect", "have no respect for", "fear", "loathe", "are disrespecting", "love", "are in awe of",
-    "are cowering in front of", "are cowering in the face of", "not taking any guff from", "are worried about",
+    "are cowering in front of", "are cowering in the face of", "are not taking any guff from", "are worried about",
     "have no fear of", "have been shamed by", "are living up to", "are going against", "are surpassing"
   ],
   "whoinrelationship":[
@@ -246,8 +247,50 @@ const baalgameCommentaryGrammar = tracery.createGrammar({
      "For #operative#"],
      "onetimepad" : ["#greeting#: #eight#", "#greeting#: #twelve#", "#greeting#: #four# Authentication #two#"]
    });
+   
+const blaseballadsGrammar = tracery.createGrammar({
+    intros : ["We are not proud to be sponsored by",
+              "We regret being sponsored by",
+              "Please support us so we won't be sponsored by",
+              "This broadcast is unfortunately sponsored by"],
+    companies : ["the Ghost Train. Ghost Train. Faster than the Styx.",
+                "hornets. Hornets. Like peanuts with wings.",
+                "Sommerland Incineration Insurance. Sommerland. Wouldn't it be a shame?",
+                "Toaster Strudel. Toaster Strudel. We're warm inside too.",
+                "the Real Las Vegas. Ignore the highway, follow I-13 to the mirage.",
+                "In-And-Out Burger. As real as California.",
+                "corvids. Corvids. When you need something more than just lots of birds.",
+                "the Commissioner. He's doing a great job.",
+                "our Guest. We wish they had something to say, but they say enough by being here.",
+                "the Black Iron Prison. The Empire Never Ended.",
+                "IKEA. IKEA. You can never have too many hex keys.",
+                "The Archangel Michael. Michael. Heaven's original umpire.",
+                "The Muse Urania. Urania. It's all in the stars, baby.",
+                "gut flora. Gut flora. Usually better than nanomachines.",
+                "Uqbar Mirror Corporation. Uqbar. Better than copulation.",
+                "People's Fund for Kaiju Rights. Godzilla is innocent.",
+                "National Park Service. NPS. Number one in missing persons.",
+                "Starbucks. Starbucks. Coffee from a fictional mermaid.",
+                "the Chicago Fire Department. CFD. We need volunteers immediately.",
+                "Daylight Savings Time. Fall back, spring forward, go for the throat.",
+                "cassowaries. Cassowaries. Lethal, efficient, beautiful.",
+                "Rowland Burrito Howitzers. Rowland. More reliable than burrito railguns.",
+                "hope. Hope. Is this making it through? Can you read me? Finally, I—",
+                "eagles. Eagles. Do you remember America?",
+                "Belson and Belson Falconry Law. B and B. Birds of a feather sue together.",
+                "Wimbledon 2. Wimbledon 2. Tennis evolved.",
+                "Joaquin and Sons Funeral Home and Accordion Repair. J and S. Wake the dead.",
+                "Cassandra. Cassandra. I told you so.",
+                "Mountain Dew. Mountain Dew. First colonized planet in the Pepsi Universe.",
+                "Blue Apron. Blue Apron. Your kitchen doesn't care about you, but we do.",
+                "the Yellowstone Supervolcano. Yellowstone. Inevitable."],
+    blaseballads : ["#intros# #companies#"] 
+  });
 
 function getCannedCommentary() {
+  if (Math.random() < 0.12) {
+    return blaseballadsGrammar.flatten("#blaseballads#");
+  }
   if (Math.random() < 0.3) {
     return nickCommentaryGrammar.flatten("#origin#");
   }
@@ -339,6 +382,10 @@ function getGameOverCommentary(game) {
   if (Math.random() < 0.166) {
     gameOverComments.push(baalgameCommentaryGrammar.flatten("#onetimepad#"));
   }
+  if (Math.random() < 0.5) {
+    gameOverComments.push(blaseballadsGrammar.flatten("#blaseballads#"));
+  }
+
 
   // time until next game
   if (Math.random() < 0.1) {
